00c4e524ebe3f1d79107c2425a4be1b42121d858
MikkPtr <cacahouettetbs@gmail.com>
2024-12-04 09:27:13 +0100

    feat: install router onto index page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import Login from './pages/login';
+import Registration from './pages/registration';
+import Xatroom from './pages/xatroom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" >
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="registration" element={<Registration />} />
+      <Route path="/xatroom/:id" element={<Xatroom />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
